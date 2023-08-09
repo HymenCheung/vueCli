054e7d9df9417688de9c57e66a070440164472db
HymenCheung <13170374+hymencheung@user.noreply.gitee.com>
2023-08-09 17:11:58 +0800

    第一次

---
+++ LIU_work/JavaEE_10-1_date0727/JavaEE-04/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<%-- 这个是jsp的注释 --%>
+<%@ page contentType="text/html;charset=utf-8" language="java">
+hello world
+
+
+<html>
+<body>
+<h2>Hello World!</h2>
+</body>
+</html>
---
+++ LIU_work/vueFrameWork_1107_date0809/js/course.js
@@ -0,0 +1,17 @@
+export  default{
+        template:
+                `<div class="course">
+                        <img src="" alt="">
+                     <div class="course-content">
+                        <div class="course-title">基于实时大数据开发</div>
+                        <div class="course-content-bottom">
+                                <img src="" alt="">
+                                <div class="mr-12">2</div>
+                                <div class="">入门</div>
+                                <div class="font-16 ml-auto color-ff9d00">￥</div>
+                                <div class="font-16 color-ff9d00">329</div>
+                        </div>
+                     </div>
+                </div>`,
+
+}
